1537d0d50607971e42f6863e4ef2af1de11b8fe0
Saad Nafie <>
2024-03-30 14:52:49 +0200

    install project - set jwt and auth

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        $data = [
+            'name' => $this->name,
+            'email' => $this->email,
+            'phone' => $this->phone,
+        ];
+        if($this->token != null){
+            $data += [
+                'access_token' => $this->token,
+                'token_type' => 'bearer',
+                'expires_in' => auth('api')->factory()->getTTL() * 60
+            ];
+        }
+        return $data;
+    }
+}
